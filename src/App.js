0f7bec3f702b5cfa72fc555433906f9e73b424e8
mlynam <mlynam@iteamnm.com>
2017-07-06 22:53:10 -0600

    feat: initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        <div className="App-header">
+          Mary Mccleary Law
+        </div>
+        <div className="App-bgBar">
+        </div>
+        <div className="App-welcome">
+          <section className="App-section">
+            <h3>Welcome to Mary Mccleary Law!</h3>
+            <p>Thank you for visiting us today.  Here's some information about us.</p>
+          </section>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
